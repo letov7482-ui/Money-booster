3e04e8125870dedce7d4aaf36914cf2b919c533c
letov7482-ui <letov7482@gmail.com>
2026-07-30 01:34:31 +0300

    Update MidpointClient.java

--- src/main/java/me/midpoint/MidpointClient.java
+++ src/main/java/me/midpoint/MidpointClient.java
@@ -15,6 +15,13 @@ public class MidpointClient implements ClientModInitializer {
         modules.add(new AutoMinerModule());
         modules.add(new AutoBuyerModule());
 
+        // Регистрация команд
+        net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback.EVENT.register(
+            (dispatcher, registryAccess) -> me.midpoint.commands.MidpointCommand.register(dispatcher)
+        );
+
         System.out.println("[Midpoint] God Mode загружен! Модулей: " + modules.size());
+        System.out.println("[Midpoint] Команды: /midpoint [модуль] on/off");
+        System.out.println("[Midpoint] Пример: /midpoint AutoMiner on");
     }
 }
